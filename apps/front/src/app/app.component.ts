113a203242aab725d06f74b41130acea58152520
rikamishiro <juliasjkls@gmail.com>
2024-03-23 12:01:52 +0000

    home e sobre: config iniciais com nx console

--- apps/front/src/app/app.component.ts
+++ apps/front/src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
-import { NxWelcomeComponent } from './nx-welcome.component';
-
 @Component({
   standalone: true,
   imports: [
     RouterModule,
-    NxWelcomeComponent,
   ],
   selector: 'app-root',
   templateUrl: './app.component.html',
