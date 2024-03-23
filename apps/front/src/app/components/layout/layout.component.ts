41589239599a12b4e3749f296cf20e0b7993618e
Julia Silveira <juliasjkls@gmail.com>
2024-03-23 14:17:58 +0000

    home: modificando layout e adicionando grid-list do material

--- apps/front/src/app/components/layout/layout.component.ts
+++ apps/front/src/app/components/layout/layout.component.ts
@@ -1,11 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { AsyncPipe } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
+
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 
@@ -15,6 +17,7 @@ import { map, shareReplay } from 'rxjs/operators';
   styleUrl: './layout.component.css',
   standalone: true,
   imports: [
+    RouterModule,
     MatToolbarModule,
     MatButtonModule,
     MatSidenavModule,
@@ -26,7 +29,7 @@ import { map, shareReplay } from 'rxjs/operators';
 export class LayoutComponent {
   private breakpointObserver = inject(BreakpointObserver);
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+  isXSmall$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.XSmall)
     .pipe(
       map(result => result.matches),
       shareReplay()
