2cd3ea245e6dce90b7e13c15d381031ba59deed7
Julia Silveira <juliasjkls@gmail.com>
2024-03-23 13:23:32 +0000

    adicionando layout e primeiras configs

--- apps/front/src/app/app.component.ts
+++ apps/front/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { LayoutComponent } from './components/layout/layout.component';
 
 @Component({
   standalone: true,
   imports: [
     RouterModule,
+    LayoutComponent,
   ],
   selector: 'app-root',
   templateUrl: './app.component.html',
